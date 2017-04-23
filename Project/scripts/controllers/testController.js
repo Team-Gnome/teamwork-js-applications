7fa6c9d6f2e9e91b74b7d379091a6046f1553281
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-04-23 09:58:57 +0300

    mandatory .js files for the routing added.
    templates.js, router.js, requester.js and testController.js files added
    in scripts folder.

---
+++ Project/scripts/controllers/testController.js
@@ -0,0 +1,11 @@
+import { load as loadTemplate } from 'templates';
+
+const $appContainer = $('#app-container');
+
+export function get(params) {
+
+    loadTemplate('test')
+        .then(template => {
+            $appContainer.html(template());
+        });
+}   
