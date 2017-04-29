6ffa3593cd35837482256be17e7c6492d835d305
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-04-29 21:11:40 +0300

    data.js file moved in the new folder 'firebase'.

--- Project/public/scripts/firebase/data.js
+++ Project/public/scripts/firebase/data.js
@@ -1,18 +1,20 @@
-import { firebaseDb as db } from 'firebaseConfig';
+import {
+    firebaseDb as db
+} from 'firebaseConfig';
 
 const defaultRef = db.ref('server/saving-data/gnome-base');
 const usersRef = defaultRef.child('users');
 
 function addNewUserInDatabase(username, firstName, lastName, hashedPassword, email) {
 
-    const user = {
-        username: {
-            firstName,
-            lastName,
-            hashedPassword,
-            email
-        }
-    };
+    const user = {};
+
+    user[username] = {
+        firstName,
+        lastName,
+        hashedPassword,
+        email
+    }
 
     return new Promise((resolve, reject) => {
         usersRef.push(user);
