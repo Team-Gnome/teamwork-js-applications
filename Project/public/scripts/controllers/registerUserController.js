13cb4b6a607787bf9b0e84b9194e427f84cc08f3
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-05-03 00:16:16 +0300

    getUserInputData() that return input data for user implemented.

--- Project/public/scripts/controllers/registerUserController.js
+++ Project/public/scripts/controllers/registerUserController.js
@@ -1,30 +1,40 @@
 import loadTemplate from 'templates';
 import { addNewUserInDatabase } from 'data';
-import User from 'userAuthentication';
+import UserAuthentificatior from 'userAuthentificatior';
 
 const $root = $('#root');
 
-export function registerUser() {
+export function getUserInputData() {
     const username = $('#input-username').val();
-    const firstName = $('#input-firstname').val();
-    const lastName = $('#input-lastname').val();
+    const firstname = $('#input-firstname').val();
+    const lastname = $('#input-lastname').val();
     const password = $('#input-password').val();
     const email = $('#input-email').val();
 
-    User.registerUser(email, password);
-
-    // addNewUserInDatabase(username, firstName, lastName, password, email)
-    //     .then( /* */ );
+    return {
+        username: username,
+        firstname: firstname,
+        lastname: lastname,
+        password: password,
+        email: email
+    };
 };
 
 export function loadHandlebars(params) {
     loadTemplate('register')
         .then(template => {
             $root.html(template)
+
+            UserAuthentificatior.initAuthStatusChange();
         })
         .then(() => {
             $('#send-btn-register').click(() => {
-                registerUser();
+                UserAuthentificatior.registerUser();
             });
+        })
+        .then(() => {
+            setTimeout(() => {
+                $root.removeClass('hidden');
+            }, 500)
         });
-}
+};
