108adbb05607ec0cff80bc6d3bc0d361160f0b6e
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-05-04 10:40:25 +0300

    Implemented functionality to add new lobby in database.

--- Project/public/scripts/firebase/data.js
+++ Project/public/scripts/firebase/data.js
@@ -1,18 +1,43 @@
 import { firebaseDb as db } from 'firebaseConfig';
+import { getLobbyInputData } from 'lobbyCreationController';
+import Lobby from 'lobby';
 
-const defaultRef = db.ref('server/saving-data/gnome-base');
-const usersRef = defaultRef.child('users');
+const defaultRef = db.ref();
 
 export function addNewUserInDatabase(uid, user) {
 
     const userObj = {};
-    const userData = JSON.stringify(user);
+    const usersRef = defaultRef.child('users');
 
     userObj[uid] = {
-        userData
-    }
+        user
+    };
 
     return new Promise((resolve, reject) => {
         usersRef.set(userObj);
+    });
+};
+
+export function addNewLobbyInDatabase(username) {
+
+    const lobbyInputData = getLobbyInputData();
+    const lobby = new Lobby(username, lobbyInputData.lobbyname, lobbyInputData.sport, lobbyInputData.location, lobbyInputData.datetime, lobbyInputData.mode);
+
+    const lobbiesRef = defaultRef.child('lobbies');
+
+    return new Promise((resolve, reject) => {
+        lobbiesRef.push(lobby);
+    });
+};
+
+export function getAllLobies() {
+    return new Promise((resolve, reject) => {
+        db.ref('/lobbies')
+            .once('value')
+            .then(function (snapshot) {
+                const lobbies = snapshot.val();
+
+                resolve(lobbies);
+            });
     });
 }
