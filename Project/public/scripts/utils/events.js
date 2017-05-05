7b9e9fd6e1d97cdbffc88f8774599faf82b63bdd
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-05-05 20:48:30 +0300

    New events added.

--- Project/public/scripts/utils/events.js
+++ Project/public/scripts/utils/events.js
@@ -1,9 +1,15 @@
 import { navigo } from 'router';
 
+import Lobby from 'lobby';
 import * as data from 'data';
 import { firebaseDb as db } from 'firebaseConfig';
 import userAuthentificator from 'userAuthentificator';
 import * as loadingScreenHandler from 'loadingScreenHandler';
+import * as inputDataHandler from 'inputDataHandler';
+
+import * as listLobbiesController from 'listLobbiesController';
+import * as joinedLobbiesController from 'joinedLobbiesController';
+import * as createdLobbiesController from 'createdLobbiesController';
 
 const events = {
     register: () => {
@@ -35,14 +41,24 @@ const events = {
     },
     createLobby: () => {
         $('#create-lobby-btn').click(() => {
-            const userId = userAuthentificator.currentUserData().uid;
+            const lobbyInputData = inputDataHandler.getLobbyInputData();
+            const uid = localStorage['authkey'];
 
-            return db.ref('/users/' + userId)
+            return db.ref('/users/' + uid)
                 .once('value')
                 .then(function (snapshot) {
                     const user = snapshot.val();
 
-                    data.addNewLobbyInDatabase(user._username)
+                    const lobby = new Lobby(
+                        user._username,
+                        lobbyInputData.lobbyname,
+                        lobbyInputData.sport,
+                        lobbyInputData.location,
+                        lobbyInputData.datetime,
+                        lobbyInputData.mode);
+
+                    data.addNewLobbyInDatabase(lobby);
+                    data.addCreatedLobbyInUserDatabase(lobby);
                 })
                 .then(() => {
                     navigo.router.navigate('#/joinLobby');
@@ -53,8 +69,80 @@ const events = {
         });
     },
     joinLobby: () => {
-        $('#join-lobby-btn').click(() => {
-            console.log(1);
+        $('.join-lobby-btn').click((ev) => {
+            const $target = $(ev.target);
+            const lobbyName = $target
+                .parent()
+                .children('.d-flex')
+                .children('.mb-1')
+                .text();
+
+            function findByName(lobby) {
+                return lobby._lobbyname === lobbyName;
+            };
+
+            const lobby = listLobbiesController.cachedLobbies.find(findByName);
+
+            data.addJoinedLobbyInUserDatabase(lobby);
+
+            $target.addClass('disabled');
+            $target.addClass('btn-success');
+            $target.text('Joined');
+            $target.removeClass('btn-primary');
+        });
+    },
+    leaveLobby: () => {
+        $('.leave-lobby-btn').click((ev) => {
+            const $target = $(ev.target);
+            const lobbyName = $target
+                .parent()
+                .children('.d-flex')
+                .children('.mb-1')
+                .text();
+
+            function findByName(lobby) {
+                return lobby._lobbyname === lobbyName;
+            };
+
+            const lobby = joinedLobbiesController.cachedJoinedLobbies.find(findByName);
+
+            data.deleteJoinedLobbyFromUserDatabase(lobby)
+                .then((callback) => {
+                    callback();
+                })
+                .then(() => {
+                    location.reload();
+                })
+        });
+    },
+    deleteLobby: () => {
+        $('.delete-lobby-btn').click((ev) => {
+            console.log('here')
+            const $target = $(ev.target);
+            const lobbyName = $target
+                .parent()
+                .children('.d-flex')
+                .children('.mb-1')
+                .text();
+
+            function findByName(lobby) {
+                return lobby._lobbyname === lobbyName;
+            };
+
+            const lobby = createdLobbiesController.cachedCreatedLobbies.find(findByName);
+
+            data.deleteLobbyFromDatabase(lobby)
+                .then((callback) => {
+                    callback();
+                });
+
+            data.deleteCreatedLobbyFromUserDatabase(lobby)
+                .then((callback) => {
+                    callback();
+                })
+                .then(() => {
+                    location.reload();
+                });
         });
     }
 };
