2d627bf3c80db300f6c285791fd60f0b61ec2894
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-05-07 23:11:15 +0300

    Simple formatting of the code

--- Project/public/scripts/models/user.js
+++ Project/public/scripts/models/user.js
@@ -12,21 +12,18 @@ export default class User {
     get username() {
         return this._username;
     };
-
     set firstname(value) {
         this._firstname = value
     };
     get firstname() {
         return this._firstname;
     };
-
     set lastname(value) {
         this._lastname = value
     };
     get lastname() {
         return this._lastname;
     };
-
     set email(value) {
         this._email = value
     };
